81cbd361d55fe452b446f69a7ec178bea559c7fa
Richard <richard.kuodis@gmail.com>
2023-09-24 20:36:48 +0300

    Add models to LFS

--- texture_synthesis_adapter/examples/preview_generation.rs
+++ texture_synthesis_adapter/examples/preview_generation.rs
@@ -1,3 +1,11 @@
+use texture_synthesis_adapter::RoomLayoutData;
+
 fn main() {
-    let 
+    let room_layout_data = RoomLayoutData {
+        lines: todo!(),
+        num_lines: todo!(),
+        room_type: todo!(),
+        wall_polygons: todo!(),
+        num_wall_polygons: todo!(),
+    };
 }
